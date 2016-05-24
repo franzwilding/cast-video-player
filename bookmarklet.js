7cb40241c63cb1c827226c329cfcab44a2ec6206
Franz Wilding <franz@2robots.at>
2016-05-24 10:30:02 +0200

    fix icon url + add load event

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,9 @@
 var link = document.createElement('link');
 link.setAttribute('rel', 'import');
-link.setAttribute('href', 'https://cdn.rawgit.com/franzwilding/cast-video-player/master/cast-control.html');
+link.setAttribute('href', 'https://rawgit.com/franzwilding/cast-video-player/master/cast-control.html');
 document.body.appendChild(link);
 window.chromeCastAlreadyLoaded = true;
-e = document.createElement('script'); e.src = 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.22/webcomponents-lite.min.js'; document.body.appendChild(e);
-e = document.createElement('cast-control'); document.body.appendChild(e);
-e.createVideoPlayButtons();
+e = document.createElement('script'); e.src = 'https://cdn.rawgit.com/webcomponents/webcomponentsjs/master/webcomponents-lite.js'; document.body.appendChild(e);
+e = document.createElement('cast-control');
+e.addEventListener("load", function(event) { e.createVideoPlayButtons(); });
+document.body.appendChild(e);
